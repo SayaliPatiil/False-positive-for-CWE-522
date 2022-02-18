485730167e3c5f161e8f4b5d1d320486568e9964
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2022-02-18 13:15:30 +0530

    Update and rename DirectObjectReference1.java to InsufficientlyProtectedCredentials.java

---
+++ InsufficientlyProtectedCredentials.java
@@ -0,0 +1,9 @@
+package javacodechecker;
+public class InsufficientlyProtectedCredentials {
+public void method1() {
+Properties prop = new Properties();
+prop.load(new FileInputStream("config.properties"));
+String password = prop.getProperty("password");
+//EMB-ISSUE: CodeIssueNames.INSUFFICIENTLY_PROTECTED_CREDENTIALS
+DriverManager.getConnection(url, usr, password);
+}}
