c6ec48bde24f11238f9f41a3d562adc37969a181
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2022-02-18 12:56:10 +0530

    Update DirectObjectReference.java

--- DirectObjectReference.java
+++ DirectObjectReference.java
@@ -1,7 +1,8 @@
-public class DirectObjectReference{
+public class DirectObjectReference1 {
 @RequestMapping(value = "/records/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 @Timed
-// EMB-ISSUE: CodeIssueNames.DIRECT_OBJECT_REFERENCE
+// EMB-ISSUE: CodeIssueNames.DIRECT_OBJECT_REFERENCE/no-detect
+@PreAuthorize("hasRole('ADMIN') OR hasRole('RecordOwner')")
 public ResponseEntity < Record > get(@PathVariable Long id) {
 return log.debug("REST request to get record" + id);
 }
